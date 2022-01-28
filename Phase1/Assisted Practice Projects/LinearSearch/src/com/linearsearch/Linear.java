c2121f34946ea8bac9f017d01fb4df2baef62e76
Shahira12345 <97025460+Shahira12345@users.noreply.github.com>
2022-01-28 13:11:33 +0530

    Commit

---
+++ Phase1/Assisted Practice Projects/LinearSearch/src/com/linearsearch/Linear.java
@@ -0,0 +1,32 @@
+package com.linearsearch;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Linear 
+{
+	static int linearSearch(int a[],int key)
+	{
+	   	 int i=0;
+	   	 for(i=0;i<a.length;i++) 
+	   	 {
+	   		 if(a[i]==key)
+	   		 {
+	   			 break;
+	   		 }
+	   		 
+	   	 }
+	   	 return i;
+	 }
+	public static void main(String[] args) throws IOException
+	{
+		 int a[]= {2,5,6,7,9,12,15,56,78};
+		 Scanner sc=new Scanner(System.in);
+	   	 System.out.println("enter a key value to search");
+	   	 int key=sc.nextInt();
+	   	 System.out.println("the position of the key value "+key+ "is at :"+linearSearch(a, key));
+	   	 
+	   	 
+	}
+	
+}
